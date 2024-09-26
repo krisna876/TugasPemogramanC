39d3d3dd51f00c35ef01e44bd49b0e9cb8d03a3a
krisna876 <krisnahamdani500@gmail.com>
2024-09-26 21:05:03 +0700

    first commit

---
+++ krisna.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    char nim[] = "2441068";
+    char nama[] = "Krisna Hamdani Ahmad";
+    char jurusan[] = "Teknik Informatika";
+    
+    
+    printf("Nim: %s\n", nim);
+    printf("Nama: %s\n", nama);
+    printf("Jurusan: %s\n", jurusan); 
+    
+    return 0;
+}
+
---
+++ oom.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+ int main() {
+ 	
+ 	int P, L, T;
+ 	P = 10;
+ 	L = 7;
+ 	T = P * L;
+ 	printf("%i", T);
+ 	return 0;
+ }
